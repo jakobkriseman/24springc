2d29426f71793512e58868d0465932b47181287a
jakobkriseman <jakobkriseman@seattleacademy.org>
2017-05-09 11:00:39 -0700

    add persistant bots

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 1515;
 
 app.use(express.static(__dirname + '/public'));
+var bots = [];
+myBot = {}
+myBot.x = 40;
+myBot.y = 100;
+myBot.r = 15;
+myBot.theta = 0;
+myBot.color = "azul";
+
+function botStatus(data){
+	bots.push(data);
+}
 
 function onConnection(socket){
-  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+	socket.broadcast.emit('drawing', data);
+	function drawAllBots(){
+		for(i=0; i<bots.length; i++){
+			socket.broadcast.emit('drawing', bots[i]);
+		}
+	}
+	function onDrawing(data){
+		console.log("onDrawing",data);
+		//socket.broadcast.emit('drawing', data);
+		botStatus(data);
+		drawAllBots();
+
+	}
+  socket.on('drawing', onDrawing);
 }
 
 io.on('connection', onConnection);
